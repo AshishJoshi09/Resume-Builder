1a059b08912aa919410384517a2dff5a59fa5c55
AshishJoshi09 <aashishjoshi559@gmail.com>
2026-06-02 08:10:30 +0530

    Remove test files

--- server/testEnv.js
+++
@@ -1,5 +0,0 @@
-import "dotenv/config";
-
-console.log("KEY:", process.env.OPENAI_API_KEY?.slice(0, 10));
-console.log("BASE:", process.env.OPENAI_BASE_URL);
-console.log("MODEL:", process.env.OPENAI_MODEL);
